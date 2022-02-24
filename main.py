acc33358185c0e8cc061b002f3ae1aa7ef3c5224
Chevon Fernando <chevonfernando10@gmail.com>
2022-02-24 12:26:51 +0530

    updated main.py with visualizing

---
+++ main.py
@@ -0,0 +1,125 @@
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import os
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+dataset_url = "DataSet/Training"
+class_names = ['St_001', 'St_002', 'St_003', 'St_004', 'St_005']
+
+
+# Dataset Creation
+def create_dataset():
+    # Train dataset
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset_url,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+
+    # Validation Dataset
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset_url,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+
+    # Classes Names
+    classNames = train_ds.class_names
+
+    print("Classes: ", classNames)  # ['St_001', 'St_002', 'St_003', 'St_004', 'St_005']
+
+    # Iterate over the dataset
+    plt.figure(figsize=(10, 10))
+    for images, labels in train_ds.take(1):
+        for i in range(9):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(class_names[labels[i]])
+            plt.axis("off")
+
+    for image_batch, labels_batch in train_ds:
+        print(image_batch.shape)
+        print(labels_batch.shape)
+        break
+
+    AUTOMOUNT = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOMOUNT)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOMOUNT)
+
+    normalization_layer = layers.Rescaling(1. / 255)
+
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
+    # Notice the pixel values are now in `[0,1]`.
+    print(np.min(first_image), np.max(first_image))
+
+    num_classes = len(class_names)
+
+    model = Sequential([
+        layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    model.summary()
+
+    epochs = 10
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs
+    )
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+
+def main():
+    create_dataset()
+
+
+if __name__ == "__main__":
+    main()
