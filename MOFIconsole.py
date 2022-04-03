2d7ec08f1828d9365c6179193d3758dec3d54b46
chevonfdz <86297885+chevonfdz@users.noreply.github.com>
2022-04-03 23:27:59 +0530

    add new changes

--- MOFIconsole.py
+++ MOFIconsole.py
@@ -1,43 +1,43 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import traceback
+
 from ClassifierMOFI import ClassifierMOFI
 
 import urllib.request
-from PIL import Image
 
 
-def main():
-    print("\t*****  MOFI - CONSOLE APPLICATION VIEW ******")
-
-    reAsk = True
+def imagePath(image_url):
     modelMOFI = ClassifierMOFI()
     class_names = ['St_001', 'St_002', 'St_003', 'St_004', 'St_005']  # Array that stores names of the classes
-    # path_to_train = os.path.abspath('DataSet/Testing/20200970_43.jpg')
-    while reAsk:
-        try:
-            imageFilePath = urllib.request.urlopen(input("\n\tEnter the path to the image: "))
-            label = modelMOFI.predict(imageFilePath)
+    try:
+        imageFilePath = urllib.request.urlopen(image_url)
+        label = modelMOFI.predict(imageFilePath)
+        # print("PREDICTED IMAGE STATUS : ", class_names[label])
+        # print("MODEL CONFIDENCE : ", modelMOFI.probabilty, " %")
+        if float(modelMOFI.probabilty) > 70:
             print("PREDICTED IMAGE STATUS : ", class_names[label])
-            print("MODEL CONFIDENCE : ", modelMOFI.probabilty, " %")
+        else:
+            print("Unknown")
+        # information = [class_names[label]]
+        # with open('MOFI_prediction.txt', 'w') as f:
+        #     for item in information:
+        #         f.write("%s\n" % item)
+        # printGraph = input("Do you want a graphical interface of the image? (Y/N): ")
+        # if printGraph.lower() == "y":
+        #     modelMOFI.printBarGraph()
+        # reply = input("Try another face image? (Y/N): ")
+        # reply = reply.lower()
+        # if reply == "y":
+        #     reAsk = True
+        # else:
+        #     print("Thank you for using the MOFI application.")
+        #     reAsk = False
+        #     break
+    except:
+        traceback.print_exc()
+        print("Invalid input please try again...")
 
-            information = [class_names[label], modelMOFI.probabilty]
-            with open('MOFI_prediction.txt', 'w') as f:
-                for item in information:
-                    f.write("%s\n" % item)
-
-            printGraph = input("Do you want a graphical interface of the image? (Y/N): ")
-            if printGraph.lower() == "y":
-                modelMOFI.printBarGraph()
-            reply = input("Try another face image? (Y/N): ")
-            reply = reply.lower()
-            if reply == "y":
-                reAsk = True
-            else:
-                print("Thank you for using the MOFI application.")
-                reAsk = False
-                break
-        except:
-            print("Invalid input please try again...")
 
 if __name__ == '__main__':
-    main()
+    imagePath('https://mofiblob.blob.core.windows.net/mofiimages/20200940_7.jpg')
